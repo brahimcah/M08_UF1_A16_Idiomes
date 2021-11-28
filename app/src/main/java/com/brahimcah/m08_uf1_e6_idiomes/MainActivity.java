1d556682f5e1f49d3c18b4d64e89616a3455a087
Brahim Chahbouni <brahimcah@gmail.com>
2021-11-28 09:14:16 +0100

    CREATE

---
+++ app/src/main/java/com/brahimcah/m08_uf1_e6_idiomes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.brahimcah.m08_uf1_e6_idiomes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
